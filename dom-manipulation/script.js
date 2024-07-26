10fe793bb8536b6a6dff4de0ea1a3dc1d66c846d
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-07-26 20:51:00 +0300

    adding localstorage and handling the login

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,21 +1,31 @@
 // catch elements---
 const quoteDisplayDiv = document.getElementById('quoteDisplay'),
-newQuoteBtn = document.getElementById('newQuote');
-// newQuoteTextInput = document.getElementById('newQuoteText'),
-// newQuoteCategoryInput = document.getElementById('newQuoteCategory');
+newQuoteBtn = document.getElementById('newQuote'),
+exportQuotesBtn = document.getElementById('exportQuotes');
 
-let quotesArray = [];
+
+let quotes = [];
 
 // variable to check if form created--
 let formCreated = false;
 
 newQuoteBtn.addEventListener('click' , showRandomQuote);
+exportQuotesBtn.addEventListener('click' , exportQuotesToJson);
 
 function showRandomQuote () {
-    if (quotesArray.length > 0) {
-        let randomQuote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
+    // declare randomQuote--
+    let randomQuote;
+
+    // check if there is quotes in local storage first---
+    if(localStorage.quotesData) {
+        quotes = JSON.parse(localStorage.quotesData);
+        randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
         quoteDisplayDiv.textContent = `${randomQuote.text} (${randomQuote.category})`;
-    } 
+    }
+    // else if (quotes.length > 0) {
+    //     randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    //     quoteDisplayDiv.textContent = `${randomQuote.text} (${randomQuote.category})`;
+    // } 
     else {
         quoteDisplayDiv.textContent = 'No quotes available. Please add a quote.';
         !formCreated && createAddQuoteForm();
@@ -32,11 +42,18 @@ function addQuote () {
     // check on values are not empty----
     if (newQuoteTextInput.value && newQuoteCategoryInput.value) {
         // get and push values---
-        quotesArray.push({text : newQuoteTextInput.value.trim() 
+        quotes.push({text : newQuoteTextInput.value.trim() 
         , category : newQuoteCategoryInput.value.trim()});
+
+        // add quotes to local storage----
+        localStorage.setItem('quotesData' , JSON.stringify(quotes));
 
         // show showRandomQuote--
         showRandomQuote();
+
+        // clear inputs--
+        newQuoteTextInput.value = '';
+        newQuoteCategoryInput.value = '';
     }
 }
 
@@ -52,4 +69,44 @@ function createAddQuoteForm() {
 
     // form created-----
     formCreated = true;
+}
+
+
+
+function exportQuotesToJson() {
+    // check if there are quotes in local storage---
+    if (localStorage.getItem('quotesData')) {
+        const data = JSON.stringify(localStorage.quotesData);
+        const blob = new Blob([data], {type : 'application/json'});
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'quotes.json';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+    }
+    else {
+        quoteDisplayDiv.textContent = 'No quotes to export';
+    }
+}
+
+
+
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+
+    fileReader.onload = function(event) {
+        const importedQuotes = JSON.parse(JSON.parse(event.target.result));
+        quotes.push(...importedQuotes);
+        saveQuotes();
+        alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+}
+
+
+function saveQuotes() {
+    localStorage.quotesData = JSON.stringify(quotes);
 }
